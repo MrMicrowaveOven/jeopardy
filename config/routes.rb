921d19cdeebb08361e3221cccdaabd2d0e8f95df
Benjamin Zagorski <benj@rocksbox.com>
2018-09-29 15:41:43 -0700

    Redoing rounds

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :games do
-    resources :rounds
-  end
+    end
   root 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
