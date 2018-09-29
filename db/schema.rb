d5747b97b160722faedb9700cf205ca424862292
Benjamin Zagorski <benj@rocksbox.com>
2018-09-29 15:57:51 -0700

    Added rounds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_222154) do
+ActiveRecord::Schema.define(version: 2018_09_29_225620) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
