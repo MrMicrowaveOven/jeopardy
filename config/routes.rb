c1fffc26d5afcec0ff0d124aafcd281ccffb8e1f
Benjamin Zagorski <benj@rocksbox.com>
2018-09-30 02:27:27 -0700

    Nested rounds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :rounds
-  resources :games
+  resources :games do
+    resources :rounds
+  end
   root 'games#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
